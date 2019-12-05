6091653222e5735b715c24a252e4a4f1e73768e5
Erick Bonilla <erick.bonilla.sojo@gmail.com>
2019-12-05 15:56:04 -0400

    header y footer editados

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,14 +4,18 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by"><a href="https://iaim.gob.ve" target="_blank">Instituto Aeropuerto Internacional de Maiquetia</a> 2019</span>
+    <span class="created-by"><a href="http://www.aeropuerto-maiquetia.com.ve/web/" target="_blank">IAIM</a> 2019</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://twitter.com/IAIM_VE" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://www.instagram.com/iaim_ve/" target="_blank" class="ion ion-social-instagram"></a>
     </div>
   `,
 })
+/*
+<<a href="#" target="_blank" class="ion ion-social-github"></a>
+<a href="#" target="_blank" class="ion ion-social-facebook"></a>
+<a href="#" target="_blank" class="ion ion-social-twitter"></a>
+<a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+*/
 export class FooterComponent {
 }
