18aa883eb3c622d4ea21138c29b55d3cd6554438
Daniel Hernandez <daniel.hernandez2908@gmail.com>
2019-12-06 09:47:55 -0400

    subiendo cambios en el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,13 @@ import { ThemeModule } from './@theme/theme.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {
+  NbSelectModule,
   NbChatModule,
   NbDatepickerModule,
   NbDialogModule,
   NbMenuModule,
   NbSidebarModule,
-  NbToastrModule,  
+  NbToastrModule,
   NbLayoutModule,
   NbInputModule,
   NbButtonModule,
@@ -26,7 +27,6 @@ import {
   NbTreeGridModule,
   NbSpinnerModule,
   NbWindowModule,
-  
 } from '@nebular/theme';
 
 import { NotadebitoComponent } from './pages/notadebito/notadebito.component';
@@ -39,9 +39,10 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
   declarations: [
     AppComponent,
      NotadebitoComponent,
-     LoginComponent    
+     LoginComponent,
     ],
   imports: [
+    NbSelectModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
