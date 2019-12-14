71185ea6d13f7d0af094f557037005b1bc0305ff
Carlos Peña <gesaodin@gmail.com>
2019-12-14 10:09:34 -0400

    Login : Loading Toaster

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { LoginService, Usuario } from '../../servicio/auth/login.service';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../@core/data/users';
+import { NbToastrService } from '@nebular/theme';
 
 
 @Component({
@@ -27,16 +28,26 @@ export class LoginComponent implements OnInit {
     loading = false;
     selectedItem: string;
     estatus: string;
+    index = 0
 
     // tslint:disable-next-line: one-line
-    constructor(private loginServicio: LoginService, private router: Router){ }
+    constructor(private loginServicio: LoginService, private router: Router, private toastrService: NbToastrService){ }
 
   ngOnInit() {
 
   }
 
+  showToast(position, status, msj) {
+    this.index += 1;
+    this.toastrService.show(
+      status || 'Success',
+      msj,
+      { position, status });
+  }
+
+
   async login () {
-    this.loading = false;
+    this.loading = true;
 
     const usr: Usuario = {
       nombre : this.nombre,
@@ -48,11 +59,15 @@ export class LoginComponent implements OnInit {
       (resp) => {
         sessionStorage.setItem('key-iaim', resp.token);
         this.router.navigateByUrl('/home/tasa');
+        
       },
       (error) => {
-        console.error('No se logro conectar...');
+        this.showToast('top-right', 'warning', 'Usuario o clave errada');
+        this.nombre = ""
+        this.clave = ""
+        this.loading = false;
       },
     );
-    this.loading = true;
+    
   }
 }
