182646766eff9224333589cdecf69f481daca215
Erick Bonilla <erick.bonilla.sojo@gmail.com>
2020-01-20 09:54:15 -0400

    reporte flipcart I

--- src/app/pages/reporte/reporte.component.ts
+++ src/app/pages/reporte/reporte.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReporteComponent implements OnInit {
 
+  flipped = false;
+  loadDosas = false;
+  loadterminal = false;
+
+  toggleView() {
+    this.flipped = !this.flipped;
+  }
+
+  loadDosasCategory() {
+    this.loadDosas = !this.loadDosas;
+    }
+
+    test() {
+      this.loadterminal = !this.loadterminal;
+      }
+
+      ChangeTab(event) {
+        if (event.tabTitle == 'DOSAS') {
+            // Action for first tab
+            this.loadDosas = !this.loadDosas;
+        }
+    }
+  
   constructor() { }
 
   ngOnInit() {
