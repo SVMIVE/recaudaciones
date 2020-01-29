609211b90714cb8802d33483a2339dcbf1620079
Carlos Peña <gesaodin@gmail.com>
2020-01-29 16:02:10 -0400

    Pagos por factura

--- src/app/pages/pagos/pagos.component.ts
+++ src/app/pages/pagos/pagos.component.ts
@@ -7,6 +7,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { NbSortDirection, NbTreeGridDataSourceBuilder, NbSortRequest, NbWindowService, NbTreeGridDataSource } from '@nebular/theme';
 import { FormControl, FormsModule } from '@angular/forms';
 import { DocumentoService } from '../../servicio/sysbase/documento.service';
+import { ClienteService } from '../../servicio/sysbase/cliente.service';
 
 interface TreeNode<T> {
   data: T;
@@ -26,17 +27,19 @@ interface FSEntry {
 }
 export interface PeriodicElement {
   Reglon: number;
-  Numero: string;
-  Forma: number;
+  Control: string;
+  Seniat: string;
   Banco: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {Reglon: 1, Numero: 'Hydrogen', Forma: 1.0079, Banco: 'H'},
-  {Reglon: 2, Numero: 'Helium', Forma: 4.0026, Banco: 'He'},
-  {Reglon: 3, Numero: 'Lithium', Forma: 6.941, Banco: 'Li'},
-  {Reglon: 4, Numero: 'Beryllium', Forma: 9.0122, Banco: 'Be'},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {Reglon: 1, Numero: 'Hydrogen', Forma: 1.0079, Banco: 'H'},
+//   {Reglon: 2, Numero: 'Helium', Forma: 4.0026, Banco: 'He'},
+//   {Reglon: 3, Numero: 'Lithium', Forma: 6.941, Banco: 'Li'},
+//   {Reglon: 4, Numero: 'Beryllium', Forma: 9.0122, Banco: 'Be'},
+// ];
+
+var ELEMENT_DATA: PeriodicElement[] = [];
 
 @Component({
   selector: 'ngx-pagos',
@@ -49,16 +52,26 @@ export class PagosComponent implements OnInit {
   @ViewChild('disabledEsc', { read: TemplateRef, static: false }) disabledEscTemplate: TemplateRef<HTMLElement>;
 
   
+  codigo = ""
+  cliente = ""
+  rif = ''
+  direccion = ''
 
   cantidad = '';
 
-  displayedColumns: string[] = ['select', 'Reglon', 'Numero', 'Forma', 'Banco'];
+
+
+  displayedColumns: string[] = ['select', 'Reglon', 'Control', 'Seniat', 'Banco'];
   dataSources = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
 
 
   constructor(private dataSourceBuilder: NbTreeGridDataSourceBuilder<FSEntry>,
-  private docu: DocumentoService, private windowService: NbWindowService) {
+    private docu: DocumentoService, 
+    private windowService: NbWindowService,
+    private servicioCliente : ClienteService) {
+
+      this.dataSources.data = ELEMENT_DATA
     
   }
 
@@ -103,5 +116,47 @@ export class PagosComponent implements OnInit {
   Procesar() {
     this.dataSources.data.forEach(row => 'console.log(row)');
     'console.log(this.selection)';
+  }
+
+
+
+  consultarCliente(id){
+
+    return this.servicioCliente.consultar(this.codigo).subscribe(
+      (resp) => { 
+        console.log(resp )
+        this.cliente = resp[0].razon_social
+        this.rif = resp[0].cedula_rif 
+        this.direccion = resp[0].dir_estado              
+       },
+      (err) => {
+          console.log(err)
+      }
+    )
+  }
+
+  lstPagos(){
+    return this.servicioCliente.lstPagos(this.codigo).subscribe(
+      (resp) => { 
+        console.log(resp )
+        var i = 0
+        resp.data.forEach(e => {
+          i++
+          ELEMENT_DATA.push( {
+            Reglon: i,
+            Control: e.nu_documento,
+            Seniat: e.nu_seniat,
+            Banco: "string"
+          } )
+        });
+       
+
+        this.dataSources.data = ELEMENT_DATA
+               
+       },
+      (err) => {
+          console.log(err)
+      }
+    )
   }
 }
