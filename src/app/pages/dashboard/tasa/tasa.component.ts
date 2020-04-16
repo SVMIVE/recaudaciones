2e190039fad4b4d2f3bde9e7daf74bab1349c447
Erick Bonilla <erick.bonilla.sojo@gmail.com>
2020-04-16 13:00:15 -0400

    corregido error en tasas

--- src/app/pages/dashboard/tasa/tasa.component.ts
+++ src/app/pages/dashboard/tasa/tasa.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TasaService } from '../../../servicio/tasa/tasa.service';
+import { CurrencyPipe } from '@angular/common'
+
 
 interface Tasa {
   descripcion : string
@@ -15,31 +17,40 @@ interface Tasa {
 export class TasaComponent implements OnInit {
 
   lstTasa = []
+  dol: any;
+  eur: any;
+  pet: any;
 
-  constructor(private tasaService : TasaService) { }
+  constructor(private tasaService : TasaService,
+    private cp: CurrencyPipe) { }
 
   ngOnInit() {
     this.tasaService.listar().subscribe(     
-      (resp) => {        
+      (resp) => {      
+        
+        this.dol = this.cp.transform(parseFloat( resp[0].ta_dollar ).toFixed(2), '', 'BS', '1.2-2','pt-BR');
+        this.eur = this.cp.transform(parseFloat( resp[0].mn_euro ).toFixed(2), '', 'BS', '1.2-2','pt-BR');
+        this.pet = this.cp.transform(parseFloat( resp[0].mn_petro ).toFixed(2), '', 'BS', '1.2-2','pt-BR');
+
         this.lstTasa = [
           {
             descripcion : "Dolar ",
             fecha : resp[0].fecha_aplica_dol.substring(0, 10),
             //monto : parseFloat( resp[0].ta_dollar ).toFixed(2) + ' Bs. ' + resp[0].fecha_aplica_dol.substring(0, 10),
-            monto : parseFloat( resp[0].ta_dollar ).toFixed(2),
+            monto : this.dol + ' ' + resp[0].fecha_aplica_dol.substring(0, 10),
           },
           {
             descripcion : "Euro ",
             fecha : resp[0].fecha_aplica_eur.substring(0, 10),
             //monto : parseFloat(  resp[0].mn_euro ).toFixed(2) + ' Bs.  ' +  resp[0].fecha_aplica_eur.substring(0, 10),
-            monto : parseFloat(  resp[0].mn_euro ).toFixed(2),
+            monto : this.eur + ' ' + resp[0].fecha_aplica_eur.substring(0, 10),
 
           },
           {
             descripcion : "Petro ",
             fecha : resp[0].fecha_aplica_ptr.substring(0, 10),
             //monto : parseFloat( resp[0].mn_petro ).toFixed(2) + ' Bs.  ' +  resp[0].fecha_aplica_ptr.substring(0, 10),
-            monto : parseFloat( resp[0].mn_petro ).toFixed(2),
+            monto : this.pet + ' ' + resp[0].fecha_aplica_ptr.substring(0, 10),
           }        
         ]
       },
